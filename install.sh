0e75fb8a9bf7a3cee611f5005367683bb2dd5453
Dmitry <waterwaterwater@users.noreply.github.com>
2020-07-09 11:56:27 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "You should be root or use sudo to run it" 
+   exit 1
+fi
+r=$(getconf LONG_BIT)
+if [ "$r" -eq "64" ]
+then
+        wget -q -O /opt/saymon-scripts/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+else 
+        wget -q -O /opt/saymon-scripts/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux32
+fi
+mkdir /opt/saymon-scripts/
+chmod 777 /opt/saymon-scripts/jq
